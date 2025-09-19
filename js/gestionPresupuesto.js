a83b7dd0acd6d977cd5e1af2d9dc35b912a4a59e
enmares <enmares2000@gmail.com>
2025-09-19 19:50:08 +0200

    Ejercicio acabado

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,56 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
+function actualizarPresupuesto(value) {
 
-function actualizarPresupuesto() {
-    // TODO
+    if(value > 0){
+        presupuesto = value;
+        return presupuesto;
+    }
+    else{
+        console.log("Error");
+    }
+    return -1;
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    console.log("Tu presupuesto actual es de " + presupuesto + "€");
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(numero, cadena) {
+    
+    let gasto = {
+    valor : numero,
+    descripcion : cadena,
+    mostrarGasto : function(){
+        console.log("Gasto correspondiente a " + this.descripcion + " con valor " + this.valor + "€");
+    },
+
+    actualizarDescripcion : function(value){
+        this.descripcion = value;
+        console.log(this.descripcion);
+    },
+
+    actualizarValor : function(value){
+        if(value > 0 && Number(value) === value){
+            this.valor = value;
+            console.log("Tu valor es " + this.valor);
+        }
+    }
 }
+    return gasto;
+}
+
+actualizarPresupuesto(4);
+mostrarPresupuesto();
+let toni = CrearGasto(90 ,"Toni");
+toni.mostrarGasto();
+toni.actualizarDescripcion("Enrique");
+toni.actualizarValor("gfasd");
+
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
