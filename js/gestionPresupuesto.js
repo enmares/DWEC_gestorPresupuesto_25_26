86a3450f59d08aefb31278f2bb3bbe6f7e01ff79
enmares <enmares2000@gmail.com>
2025-10-01 19:14:11 +0200

    Mitad del ejercico 2JS

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,4 +1,4 @@
-/*
+
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
@@ -6,125 +6,6 @@
 let presupuesto = 0;
 let gastos = [];
 let idgastos = 0;
-
-function actualizarPresupuesto(value) {
-
-    if(value > 0){
-        presupuesto = value;
-        return presupuesto;
-    }
-    else{
-        console.log("Error");
-    }
-    return -1;
-}
-
-function mostrarPresupuesto() {
-    console.log("Tu presupuesto actual es de " + presupuesto + "€");
-}
-
-function CrearGasto(valor, descripcion, tags) {
-    
-    let gasto = {
-    valor : valor,
-    descripcion : descripcion,
-    etiquetas : tags,
-    fecha : new Date().getTime(),
-
-    actualizarDescripcion : function(value){
-        this.descripcion = value;
-        console.log(this.descripcion);
-    },
-
-    actualizarValor : function(value){
-        if(value > 0 && Number(value) === value){
-            this.valor = value;
-            console.log("Tu valor es " + this.valor);
-        }
-    },
-
-    mostrarGastoCompleto : function(){
-        console.log("Gasto correspondiente a " + this.descripcion + "con valor " + this.valor + "€");
-    }
-    
-}
-    return gasto;
-}*/
-
-/////////////////PRUEBAS OBSOLETAS//////////////////////////////////
-/*actualizarPresupuesto(4);
-mostrarPresupuesto();
-let toni = CrearGasto(90 ,"Toni");
-toni.mostrarGasto();
-toni.actualizarDescripcion("Enrique");
-toni.actualizarValor("gfasd");*/
-///////////////////////////////////////////////////////////////////
-/*
-function listarGastos(){
-    return gastos;
-};
-
-function anyadirGasto(objetoGasto){
-
-    objetoGasto.id = idgastos;
-    idgastos++;
-    gastos.push(objetoGasto);
-};
-
-function borrarGasto(idGasto){
-
-    if(typeof idGasto != 'number' || idGasto < 0){
-        return;
-    }
-
-    gastos.splice(idGasto,1);
-
-};
-function calcularTotalGastos(){
-
-    let aux=0;
-    let sum=0;
-
-    for(let i=0; i < gastos.length; i++){
-        aux = gastos[i].valor;
-        sum += aux;
-    }
-    return sum;
-};
-
-function calcularBalance(){
-    let gastosTotales = calcularTotalGastos();
-    return presupuesto - gastosTotales;
-};*/
-
-/*
-let gasto1 = CrearGasto(15, "Antonio", 4);
-let gasto2 = CrearGasto(92, "dsads", 5);
-anyadirGasto(gasto1);
-anyadirGasto(gasto2);
-presupuesto= 124;
-console.log(calcularTotalGastos());
-console.log(calcularBalance());*/
-
-/*
-// NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
-// Las funciones y objetos deben tener los nombres que se indican en el enunciado
-// Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
-    mostrarPresupuesto,
-    actualizarPresupuesto,
-    CrearGasto,
-    listarGastos,
-    anyadirGasto,
-    borrarGasto,
-    calcularTotalGastos,
-    calcularBalance
-}*/
-
-// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
-
-// TODO: Variable global
-let presupuesto = 0;
 
 function actualizarPresupuesto(valor) {
 
@@ -140,8 +21,9 @@ function mostrarPresupuesto() {
     return "Tu presupuesto actual es de "+ presupuesto +" €";
 }
 
-function CrearGasto(descripcion, valor) {
+function CrearGasto(descripcion, valor, tags) {
 
+    this.etiquetas = tags;
     this.descripcion = descripcion;
 
     if(valor > 0)
@@ -162,7 +44,77 @@ function CrearGasto(descripcion, valor) {
     this.actualizarDescripcion = function(desc){
         this.descripcion = desc;
     }
+
+    this.anyadirEtiquetas = function(...arrayEtiquetas){
+        for(let i=0; i < arrayEtiquetas.length; i++)
+        {
+            if(!this.etiquetas.includes(arrayEtiquetas[i])){
+                this.etiquetas.push(arrayEtiquetas[i]);
+            }   
+        }
+    }
+
+    this.borrarEtiquetas = function(...arrayEtiquetas){
+
+        let index = 0; /*CONTINUAR POR AQUÍ!!!!*/
+
+        for(let i=arrayEtiquetas.length -1; i >= 0; i--)
+        {
+
+            if(this.etiquetas.includes(arrayEtiquetas[i])){
+                this.etiquetas.splice(index,1);
+            }
+        }
+    }
 }
+
+function listarGastos(){
+    return gastos;
+};
+
+function anyadirGasto(objetoGasto){
+
+    objetoGasto.id = idgastos;
+    idgastos++;
+    gastos.push(objetoGasto);
+};
+
+function borrarGasto(idGasto){
+
+    for(let i=gastos.length -1; i >= 0; i--)
+    {
+        if(gastos[i].id === idGasto)
+        {
+            gastos.splice(i,1);
+        }
+    }
+    return;
+};
+function calcularTotalGastos(){
+
+    let aux=0;
+    let sum=0;
+
+    for(let i=0; i < gastos.length; i++){
+        aux = gastos[i].valor;
+        sum += aux;
+    }
+    return sum;
+};
+
+function calcularBalance(){
+    let gastosTotales = calcularTotalGastos();
+    return presupuesto - gastosTotales;
+};
+
+/*
+let gasto1 = new CrearGasto("Antonio", 4);
+let gasto2 = new CrearGasto("dsads", 5);
+console.log(gasto1.valor);
+anyadirGasto(gasto1);
+anyadirGasto(gasto2);
+presupuesto= 124;*/
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
@@ -170,7 +122,12 @@ function CrearGasto(descripcion, valor) {
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
 
 
